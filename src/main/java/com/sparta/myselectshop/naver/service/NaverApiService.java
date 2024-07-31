3f2668cb0ad108ac497d6794410bfc41b71a1fef
asd42270 <lyb991113@gmail.com>
2024-07-31 17:53:39 +0900

    최초 커밋

---
+++ src/main/java/com/sparta/myselectshop/naver/service/NaverApiService.java
@@ -0,0 +1,67 @@
+package com.sparta.myselectshop.naver.service;
+
+import com.sparta.myselectshop.naver.dto.ItemDto;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@Slf4j(topic = "NAVER API 호출")
+public class NaverApiService {
+
+    private final RestTemplate restTemplate;
+
+    public NaverApiService(RestTemplateBuilder builder) {
+        this.restTemplate = builder.build();
+    }
+
+    public List<ItemDto> searchItems(String query) throws JSONException {
+
+        URI uri = UriComponentsBuilder
+                .fromUriString("https://openapi.naver.com")
+                .path("/v1/search/shop.json")
+                .queryParam("display", 15)
+                .queryParam("query", query)
+                .encode()
+                .build()
+                .toUri();
+        log.info("uri = " + uri);
+
+        RequestEntity<Void> request = RequestEntity
+                .get(uri)
+                .header("X-Naver-Client-Id", "2EicTBuR_92N1ieqbAaY")
+                .header("X-Naver-Client-Secret", "qrQYwYYxS1")
+                .build();
+
+        ResponseEntity<String> response = restTemplate.exchange(request, String.class);
+
+        return fromJsonToItems(response.getBody());
+    }
+
+    private List<ItemDto> fromJsonToItems(String body) throws JSONException {
+        JSONObject jsonObject = new JSONObject(body);
+
+        JSONArray jsonArray = jsonObject.getJSONArray("items");
+        List<ItemDto> items = new ArrayList<>();
+
+        for (int i = 0; i < jsonArray.length(); i++) {
+            JSONObject itemJson = jsonArray.getJSONObject(i);
+            ItemDto itemDto = new ItemDto(itemJson);
+            items.add(itemDto);
+        }
+
+        return items;
+    }
+}
---
+++ src/main/java/com/sparta/myselectshop/product/entity/Product.java
@@ -0,0 +1,36 @@
+package com.sparta.myselectshop.product.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "products")
+@Getter
+@NoArgsConstructor
+@Builder
+@AllArgsConstructor
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String image;
+
+    @Column(nullable = false)
+    private String link;
+
+    @Column(nullable = false)
+    private Integer lPrice;
+
+    @Column(nullable = false)
+    private Integer myPrice;
+
+}
